53d06b161c808b365697d76426c77ae0cf00644f
10192096 <10192096@A23279516.zte.intra>
2016-04-16 12:45:13 +0800

    111111111111111111111111111

--- src/com/JG/common/Constans.java
+++ src/com/JG/common/Constans.java
@@ -2,4 +2,12 @@ package com.JG.common;
 
 public class Constans {
 	public static final String ZIP_FILE_ABSOLUTELY_PATH= "zipFileAbsolutelyPath";
+	
+	public static final String DEST_DIR = "dest_dir";
+	public static final String UNZIPFILE_LIST = "unzipFileList";
+	public static final String EXCLUDEFILE_LIST = "excludeFileList";
+	
+	public static final String XML_DESTINATION = "destination";
+	public static final String XML_UNZIPFILE = "unzipFile";
+	public static final String XML_EXCLUDECOVERY = "excludeCovery";
 }
--- src/com/JG/main/MainGUI.java
+++ src/com/JG/main/MainGUI.java
@@ -1,25 +1,72 @@
 package com.JG.main;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 import com.JG.common.Constans;
+import com.JG.util.FileUtil;
 
 public class MainGUI {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] arg) throws Exception{
-//		System.out.println(arg.length);
 		String zipFileAbsolutePath = null;
 		if(arg.length>0){
 			zipFileAbsolutePath = arg[0];
+		}else{
+			System.out.println("");
+			return;
 		}
-		Map<String,String> map = new HashMap<String,String>();
-		map.put(Constans.ZIP_FILE_ABSOLUTELY_PATH, zipFileAbsolutePath);
-		JFrame mainFrame = new MainFrameGUI(map);
-		UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
-		SwingUtilities.updateComponentTreeUI(mainFrame.getContentPane());
+		Map<String,Object> map = getXMLInfo(new File("D:\\tempFile\\base.xml"));
+		Set<String> unZipSet = new HashSet<String>((List<String>)map.get(Constans.XML_UNZIPFILE));
+		Set<String> excludeFileSet = new HashSet<String>((List<String>)map.get(Constans.XML_EXCLUDECOVERY));
+		
+		File unzipFileSource = new File(zipFileAbsolutePath);
+		File unzipFileDest = new File(unzipFileSource.getAbsolutePath()+File.separator);
+		unzipFileDest.mkdirs();
+		FileUtil.unzipFile(unzipFileSource, unzipFileDest, unZipSet);
+		FileUtil.coverFile(unzipFileDest, new File((String)map.get(Constans.XML_DESTINATION)), excludeFileSet);
+	}
+	
+	/**
+	 * 解析xml中的文件路径信息
+	 * @author Ji Cheng 
+	 * @date 2016年4月16日
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 * @return Map<String,Object>
+	 */
+	public static Map<String,Object> getXMLInfo(File file) throws Exception{
+		Map<String,Object> resultMap = new HashMap<String,Object>();
+		SAXReader reader = new SAXReader();
+		Document doc = reader.read(file);
+		Element root = doc.getRootElement();
+		
+		resultMap.put(Constans.DEST_DIR, root.elementText(Constans.XML_DESTINATION));
+		Element zipListEl = root.element(Constans.XML_UNZIPFILE);
+		List<String> unzipFileList = new ArrayList<String>();
+		for(int i=0;zipListEl.elements()!=null&&i<zipListEl.elements().size();i++){
+			Element tempEl = (Element)zipListEl.elements().get(i);
+			unzipFileList.add(tempEl.getText());
+		}
+		resultMap.put(Constans.UNZIPFILE_LIST, unzipFileList);
+		
+		Element exludeListEl = root.element(Constans.XML_EXCLUDECOVERY);
+		List<String> excludeFileList = new ArrayList<String>();
+		for(int i=0;exludeListEl.elements()!=null&&i<exludeListEl.elements().size();i++){
+			Element tempEl = (Element)exludeListEl.elements().get(i);
+			excludeFileList.add(tempEl.getText());
+		}
+		resultMap.put(Constans.EXCLUDEFILE_LIST, excludeFileList);
+		return resultMap;
 	}
 }
